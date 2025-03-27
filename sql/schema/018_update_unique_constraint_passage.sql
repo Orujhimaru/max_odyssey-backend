0361e07c843d5c5e71eeb5eb788afda7b9c60cd8
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2025-03-27 21:34:33 +0400

    db: added some math values

--- sql/schema/018_update_unique_constraint_passage.sql
+++ sql/schema/018_update_unique_constraint_passage.sql
@@ -4,4 +4,4 @@ DROP CONSTRAINT IF EXISTS unique_question;
 
 -- Add a new unique constraint using passage instead of question_text
 ALTER TABLE questions 
-ADD CONSTRAINT unique_question UNIQUE (passage, topic, subtopic); 
+ADD CONSTRAINT unique_question UNIQUE (question_text, passage, topic, subtopic); 
